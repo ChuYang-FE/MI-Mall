d2be43e8f5b0f6d61983c6336b2620fe8565f9f8
young <865428527@qq.com>
2020-07-24 17:39:21 +0800

    init build 基本架构

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  components: {
+    
+  },
+  data(){
+    return {
+      
+    }
+  },
+  mounted(){
+    
+  },
+  methods:{
+    
+  }
+}
+</script>
+
+<style lang="scss">
+@import './assets/scss/reset.scss';
+@import './assets/scss/config.scss';
+@import './assets/scss/button.scss';
+</style>
---
+++ src/pages/home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <nav-header></nav-header>
+    <router-view></router-view>
+    <nav-footer></nav-footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'nav-footer',
+  components: {
+    
+},
+  data() {
+    return {
+      
+    }
+},
+//生命周期 - 创建完成（访问当前this实例）
+  created() {
+    
+  },
+//生命周期 - 挂载完成（访问DOM元素）
+  mounted() {
+    
+  }
+}
+</script>
+
+<style scoped>
+/* @import url(); 引入css类 */
+
+</style>
