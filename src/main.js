31c8209bd92563c9f0b32a1d29df801036f11f77
young <865428527@qq.com>
2020-07-25 17:41:39 +0800

    环境的设置

--- src/main.js
+++ src/main.js
@@ -1,8 +1,31 @@
 import Vue from 'vue'
-import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import router from './router.js'
+import App from './App.vue'
+import env from './env.js'
 
+Vue.use(axios, VueAxios);
 Vue.config.productionTip = false;
+
+// 根据前端的跨域方式做调整 /a/b : /api/a/b => /a/b
+axios.defaults.baseURL = '/api';
+axios.defaults.timeout = 8000; 
+
+//根据环境变量获取不同的请求地址
+axios.defaults.baseURL = env.baseURL;
+
+//接口错误拦截
+axios.interceptors.response.use(function(response){
+  let res = response.data;
+  if(res.status == 0){  //主要看接口约定，status是0代表成功，10代表未登录。
+    return res.data;
+  } else if(res.status == 10) {
+    window.location.href = "/#/login";
+  } else {
+    alert(res.msg);
+  }
+})
 
 new Vue({
   router,
