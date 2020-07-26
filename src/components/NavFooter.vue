2c4fae4a0ed724cde2118172bd37ee4835c36dc8
young <865428527@qq.com>
2020-07-26 21:21:21 +0800

    footer组件开发

--- src/components/NavFooter.vue
+++ src/components/NavFooter.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>
+  <div class="footer">
+    <div class="footer-logo">
+      <img src="/imgs/logo-footer.png" alt="">
+      <p>小米商城</p>
+    </div>    
+    <div class="footer-link">
+      <a href="https://github.com/CoderTyy" target="_blank">谭小羊的个人主页</a><span>|</span>
+      <a href="https://github.com/CoderTyy/MI-Mall" target="_blank">PC端-小米商城</a><span>|</span>
+      <a href="https://github.com/CoderTyy/Vue-travel" target="_blank">移动端-去哪儿网</a><span>|</span>
+      <a href="https://github.com/CoderTyy/JDFinance" target="_blank">移动端-京东金融</a>
+    </div>
+    <div class="copyright">Copyright @2020 <span class="domain">谭小羊</span> All Rights Reserved.</div>
   </div>
 </template>
 
@@ -25,7 +36,39 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 /* @import url(); 引入css类 */
-
+// @import '';
+  .footer{
+    height:234px;
+    border-top:4px solid #FF6600;
+    background-color:#333333;
+    color:#999999;
+    font-size:16px;
+    text-align:center;
+    .footer-logo{
+      margin-top:46px;
+      margin-bottom:31px;
+      img{
+        width:53px;
+        height:36px;
+        margin-bottom:13px;
+      }
+    }
+    .footer-link{
+      a{
+        color:#999999;
+        display:inline-block;
+      }
+      span{
+        margin:0 10px;
+      }
+      margin-bottom:13px;
+    }
+    .copyright{
+      .domain{
+        color:#FF6600;
+      }
+    }
+  }
 </style>
