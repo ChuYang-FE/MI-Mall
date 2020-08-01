b08ff93262ae5ff3fbc47172d51e6bf9e7800943
young <865428527@qq.com>
2020-08-01 16:08:25 +0800

    购物车商品勾选、数量、价格统计

--- src/components/OrderHeader.vue
+++ src/components/OrderHeader.vue
@@ -1,32 +1,63 @@
 <template>
-  <div>
-    orderHeader
+  <div class="order-header">
+    <div class="container clearfix">
+      <div class="header-logo">
+        <a href="/#/index"></a>
+      </div>
+      <div class="title">
+        <h2>{{title}}<slot name="tip"></slot></h2>
+      </div>
+      <div class="username">
+        <a href="javascript:;">{{username}}</a>
+      </div>
+    </div>
   </div>
 </template>
-
 <script>
-export default {
-  name: 'order-header',
-  components: {
-    
-},
-  data() {
-    return {
-      
-    }
-},
-//生命周期 - 创建完成（访问当前this实例）
-  created() {
-    
-  },
-//生命周期 - 挂载完成（访问DOM元素）
-  mounted() {
-    
+  import {mapState} from 'vuex'
+  export default{
+    name:'order-header',
+    props:{
+      title:String
+    },
+    computed:{
+      ...mapState(['username'])
+    },
   }
-}
 </script>
-
-<style scoped>
-/* @import url(); 引入css类 */
-
+<style lang="scss">
+  .order-header{
+    padding:30px 0;
+    border-bottom:2px solid #FF6600;
+    .header-logo{
+      float:left;
+      overflow: hidden;
+    }
+    .title,.username{
+      display:inline-block;
+      height:55px;
+      line-height:55px;
+    }
+    .title{
+      float:left;
+      margin-left:54px;
+      h2{
+        font-size:28px;
+        color:#333333;
+      }
+      span{
+        font-size:14px;
+        margin-left:17px;
+        color:#999999;
+        font-weight:200;
+      }
+    }
+    .username{
+      float:right;
+      a{
+        color:#666666;
+        font-size:16px;
+      }
+    }
+  }
 </style>
